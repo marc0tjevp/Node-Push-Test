7c379532a0fc7b1f1130ca212a036184e4d7493b
Marco van Poortvliet <Marc0tjevp@gmail.com>
2019-02-12 00:11:27 +0100

    Sends notification

--- client/worker.js
+++ client/worker.js
@@ -1,10 +1,11 @@
 self.addEventListener('push', e => {
+    
     const data = e.data.json()
 
     // Show notification
     self.registration.showNotification(data.title, {
         body: 'Web Push Test',
-        icon: 'https://media.tenor.com/images/805fad219d83c53896d7681d87732d1b/raw'
+        icon: 'https://png.pngtree.com/svg/20170719/icon_message_notification_351570.png'
     })
 
 })
